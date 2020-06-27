313eb6b3a66b34044808b425a09dce0e8953754d
Abhishek Guntaka <abhi91420@gmail.com>
2020-06-26 19:17:07 -0700

    implimented loginDetails and bankaccount class creation

---
+++ src/BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private String accountHolder;
+    private String accountNo;
+    private long accountBalance;
+    public void setAccountHolder(String accountHolder){
+        this.accountHolder = accountHolder;
+    }
+    public void setAccountNo(String accountNo){
+        this.accountNo=accountNo;
+    }
+    public void setAccountBalance(long accountBalance){
+        this.accountBalance = accountBalance;
+    }
+    public String getAccountHolder(){
+        return this.accountHolder;
+    }
+    public String getAccountNo(){
+        return this.accountNo;
+    }
+    public long getAccountBalance(){
+        return this.accountBalance;
+    }
+    public void deposit(long depositAmount){
+        this.accountBalance +=depositAmount;
+    }
+    public void withdraw(long withdrawAmount){
+        this.accountBalance -= withdrawAmount;
+    }
+}
